59f3bc08e7b21ebd4b0dcecc86bc1980620a0f0b
Daro H <dario.hruszecki@gmail.com>
2023-07-04 18:57:05 -0300

    docker support

--- src/main/java/org/dnh/todolist/entity/Task.java
+++ src/main/java/org/dnh/todolist/entity/Task.java
@@ -6,12 +6,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import jakarta.persistence.GeneratedValue;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Task {
     @Id
